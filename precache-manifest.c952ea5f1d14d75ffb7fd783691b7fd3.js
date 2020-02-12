7927fabfe7dbae1d7cf6cce94da573b618c129dc
Rúben Rebelo <rav.rebelo@campus.fct.unl.pt>
2020-02-12 10:08:29 +0000

    Updates

---
+++ precache-manifest.c952ea5f1d14d75ffb7fd783691b7fd3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b859bf321cc380e022854bd414a7f439",
+    "url": "/igive/index.html"
+  },
+  {
+    "revision": "39424e5f77718d51bf4f",
+    "url": "/igive/static/css/main.900e1737.chunk.css"
+  },
+  {
+    "revision": "b3939e10543720e98153",
+    "url": "/igive/static/js/2.059271ab.chunk.js"
+  },
+  {
+    "revision": "39424e5f77718d51bf4f",
+    "url": "/igive/static/js/main.73f77ba4.chunk.js"
+  },
+  {
+    "revision": "3e36ee961451444da0e1",
+    "url": "/igive/static/js/runtime~main.22ebfc59.js"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/igive/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "6b1983a26a32647c4832456dc9f5dcd3",
+    "url": "/igive/static/media/bg1.6b1983a2.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/igive/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/igive/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/igive/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/igive/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/igive/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/igive/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/igive/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/igive/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/igive/static/media/ryan.f217f346.jpg"
+  }
+]);
